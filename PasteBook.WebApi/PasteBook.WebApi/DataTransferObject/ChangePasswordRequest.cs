c49f486d145e07dab7fceb3061674d3997f3f571
ElwardJCrianza <elward.crianza@pointwest.com.ph>
2022-05-16 15:08:47 +0800

    NewsFeed Refresh, EditAcctInfo, AuthDbAdjustments - Ward

--- PasteBook.WebApi/PasteBook.WebApi/DataTransferObject/ChangePasswordRequest.cs
+++ PasteBook.WebApi/PasteBook.WebApi/DataTransferObject/ChangePasswordRequest.cs
@@ -3,7 +3,7 @@ namespace PasteBook.WebApi.DataTransferObject
 {
     public class ChangePasswordRequest
     {
-        public int AuthId { get; set; }
+        public int UserId { get; set; }
         public string? EmailAddress { get; set; }
         public string? NewPassword { get; set; }
         public string OldPassword { get; set; }
